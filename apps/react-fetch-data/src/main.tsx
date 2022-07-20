bdc5af4a0027e5bc5d3f1dda09d4c03f71bc7e41
Cristi <ratoi_crysty@yahoo.com>
2022-07-20 10:51:08 +0300

    feat: remove strict mode from react

--- apps/react-fetch-data/src/main.tsx
+++ apps/react-fetch-data/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './app/app';
 import { BrowserRouter } from 'react-router-dom';
@@ -7,9 +6,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
 );
